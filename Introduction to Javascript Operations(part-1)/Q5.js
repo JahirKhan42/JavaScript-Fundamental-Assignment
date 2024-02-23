47a3a138210c7daeea6b42d025d8703168a52267
Jahir ali khan <102781431+JahirKhan42@users.noreply.github.com>
2024-02-23 22:27:50 +0530

    Create Q5.js

---
+++ Introduction to Javascript Operations(part-1)/Q5.js
@@ -0,0 +1,5 @@
+let principal = 1000; // Principle amount in dollars
+let rate = 5; // Rate of interest in percentage
+let time = 2; // Time period in years
+let simpleInterest = (principal * rate * time) / 100;
+console.log("Simple Interest:", simpleInterest );
